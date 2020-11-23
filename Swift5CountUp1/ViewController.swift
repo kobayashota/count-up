1beeafcfcf735b9164b96a79dc40be6bc2e0ba06
kobashou06 <kobashou06@gmail.com>
2020-11-24 06:50:00 +0900

    created views background image count minus button count plus button

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // ラベルの文字を０と表示したい
+        countLabel.text = "0"
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        
+        //カウントアップさせたい
+        
+        //ラベルに文字を反映したい（数字）
+        
+        //色を１０以上になったら黄色に変更したい
+        
+    }
+    @IBAction func minus(_ sender: Any) {
+        
+        //カウントダウンさせたい
+        
+        //ラベルに文字を反映したい（数字）
+        
+        //色を０以下になったら白色に変更したい
+        
+    }
+    
 }
 
